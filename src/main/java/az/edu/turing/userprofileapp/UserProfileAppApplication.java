095f71bd514a345b1afe8755fa41e608242d1659
HP ProBook <kalbaliyevaselen@gmai.com>
2024-08-08 11:37:41 +0400

    first commit

---
+++ src/main/java/az/edu/turing/userprofileapp/UserProfileAppApplication.java
@@ -0,0 +1,13 @@
+package az.edu.turing.userprofileapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserProfileAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserProfileAppApplication.class, args);
+    }
+
+}
