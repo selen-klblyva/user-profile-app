4b34fd445d9dd15f4a0f6c7fb4b9eb4d4fff433a
HP ProBook <kalbaliyevaselen@gmai.com>
2024-08-09 16:43:33 +0400

    third commit

--- src/main/java/az/edu/turing/userprofileapp/feign/UserFeignClient.java
+++ src/main/java/az/edu/turing/userprofileapp/feign/UserFeignClient.java
@@ -3,12 +3,13 @@ package az.edu.turing.userprofileapp.feign;
 
 import az.edu.turing.userprofileapp.model.dto.UserDto;
 import az.edu.turing.userprofileapp.model.entity.UserEntity;
+import jakarta.persistence.metamodel.SingularAttribute;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.data.jpa.domain.AbstractPersistable;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.Serializable;
 import java.util.List;
-
-import static org.springframework.data.jpa.domain.AbstractPersistable_.id;
 
 @FeignClient(name = "userService", url = "http://localhost:8082")
 public interface UserFeignClient {
@@ -18,19 +19,19 @@ public interface UserFeignClient {
     List<UserDto> getUsers();
 
     @GetMapping("/users/{id}")
-    UserDto getUserById(@PathVariable("id") Long userId);
+    UserEntity getById(@PathVariable("id") Long userId);
 
     @GetMapping
-    int getCountOfUsers();
+    int getCountOfUsers(SingularAttribute<AbstractPersistable, Serializable> id);
 
     @PutMapping("/users/{id}")
-    UserEntity updateUser(@PathVariable("id") Long userId, @RequestBody UserEntity userEntity);
+    UserEntity updateUser(@RequestBody UserEntity userEntity);
 
     @PostMapping("/users")
-    UserDto createUser(@RequestBody UserDto userDto);
+    UserDto createUser(@RequestBody UserEntity userDto);
 
     @PatchMapping("/users/{age}")
-    UserEntity updateUserAge(@PathVariable("id") Long userId, @RequestBody UserEntity userEntity);
+    UserEntity updateUserAge(@RequestBody UserEntity userEntity);
 
     @DeleteMapping("/users/{id}")
     void deleteUserById(@PathVariable("id") Long userIdd);
--- src/main/java/az/edu/turing/userprofileapp/service/UserServiceImpl.java
+++ src/main/java/az/edu/turing/userprofileapp/service/UserServiceImpl.java
@@ -5,53 +5,90 @@ import az.edu.turing.userprofileapp.feign.UserFeignClient;
 import az.edu.turing.userprofileapp.model.dto.UserDto;
 import az.edu.turing.userprofileapp.model.entity.UserEntity;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import static org.springframework.data.jpa.domain.AbstractPersistable_.id;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class UserServiceImpl implements UserService {
 
     private static  UserFeignClient userFeignClient;
 
     @Override
     public void save(UserDto userDto) {
+        UserEntity userEntity=UserEntity.builder()
+                .username(userDto.getUsername())
+                .age(userDto.getAge())
+                .createdAt(LocalDateTime.now())
+                .updatedAt(LocalDateTime.now())
+                .build();
+                userFeignClient.createUser(userEntity);
+        log.info("User saved: {}", userEntity);
 
     }
 
     @Override
     public List<UserDto> getAllUsers() {
-        return List.of();
+        return userFeignClient.getUsers().stream()
+                .map(entity -> new UserDto(entity.getUserId(),entity.getUsername(),
+                        entity.getAge(),entity.getCreatedAt(),entity.getUpdatedAt()))
+                .collect(Collectors.toList());
     }
 
     @Override
     public UserDto getUserById(long userId) {
-        return null;
+            UserEntity userEntity=userFeignClient.getById(userId);
+            if(userEntity==null){
+                log.error("User not found");
+            }
+
+            return new UserDto(userEntity.getUserId(),userEntity.getUsername(),
+                    userEntity.getAge(),userEntity.getCreatedAt(),userEntity.getUpdatedAt());
     }
 
     @Override
     public void deleteAll() {
-
+        userFeignClient.deleteUsers();
     }
 
     @Override
     public void deleteById(long userId) {
-
+         userFeignClient.deleteUserById(userId);
     }
+
 
     @Override
     public UserEntity update(Long userId, UserEntity userEntity) throws UserNotFoundException {
-        return null;
+        UserEntity existingUser = userFeignClient.getById(userId);
+        existingUser.setUsername(userEntity.getUsername());
+        existingUser.setAge(userEntity.getAge());
+        existingUser.setCreatedAt(LocalDateTime.now());
+        existingUser.setUpdatedAt(LocalDateTime.now());
+        userFeignClient.updateUser(existingUser);
+        log.info("User updated with id: {}", userId);
+        return existingUser;
     }
+
 
     @Override
     public UserEntity updateAge(Long userId, UserEntity userEntity) throws UserNotFoundException {
-        return null;
+        UserEntity existingUser = userFeignClient.getById(userId);
+        existingUser.setAge(userEntity.getAge());
+        userFeignClient.updateUserAge(existingUser);
+        log.info("User age updated with id: {}", id);
+        return existingUser;
     }
 
     @Override
     public int getCountOfAllUsers() {
-        return 0;
+        return userFeignClient.getCountOfUsers(id);
     }
 }
